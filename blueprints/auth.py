340516e17e0b8f85e619ab5a66994cb4a76f4e38
高宇声 <976631565@qq.com>
2023-09-05 23:06:40 +0800

    项目初始化0905

---
+++ blueprints/auth.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+bp = Blueprint("auth",__name__,url_prefix="/auth")
+
+@bp.route("/login")
+def login():
+    pass
---
+++ exts.py
@@ -0,0 +1,5 @@
+# 这个文件的意义是解决循环引用的问题
+# flask-sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
